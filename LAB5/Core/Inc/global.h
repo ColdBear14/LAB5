bdff98990641f4585743a16bd6143d6aa4617ea1
LAPTOP-L4QLQTHA\Hy <ngxuanhy1405@gmail.com>
2024-11-19 22:37:19 +0700

    update lab5

---
+++ LAB5/Core/Inc/global.h
@@ -0,0 +1,23 @@
+/*
+ * global.h
+ *
+ *  Created on: Nov 19, 2024
+ *      Author: Hy
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include "main.h"
+
+#define MAX_BUFFER_SIZE	30
+extern uint8_t temp;
+extern uint8_t buffer[];
+extern uint8_t index_buffer;
+extern uint8_t buffer_flag;
+
+extern uint8_t command_flag;
+extern uint8_t command_data[];
+extern uint32_t ADC_value;
+
+#endif /* INC_GLOBAL_H_ */
